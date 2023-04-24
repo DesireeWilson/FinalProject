b7cc90a0006b2e471db6f0396815c8cc27026151
Desiree Wilson <desiree.s.wilson@gmail.com>
2023-04-23 19:26:43 -0500

    Final analyses for this project
    Here, I performed modified t-test on the probes measuring the expression of 29 genes: MYBPC1, SLC7A4, CDC42EP5, BCAS1, PAK1IP1, ANPEP, SLC23A1, HS.270778, GLB1L2, GCNT2, TNFRSF19, CUX2, POTEG, NCAPD3, LOC728606, SLC22A3, C9ORF61, SRD5A2, FLJ31568, SERPINF2, PGM5, LOC645993, ZDHHC8P, CNTNAP2, F12, CXCL14, CTHRC1, ZNF467, CDC20. I created box plots looking at the log2 normalized expression of the probes in Gleason 3+4 and Gleason 4+3 cancers and unfortuntely NONE of the probes are statistically significantly different. Additionally, I performed a survival analysis, only looking at time to biochemical recurrence in Gleason 3+4 verses Gleason 4+3 cases. And unfortunately, there is no difference in time to biochemical recurrence in Gleason 3+4 verses 4+3 cases. :(

--- GSE70769/Scripts/GSE70769Scripts.R
+++ GSE70769/Scripts/GSE70769Scripts.R
@@ -1,5 +1,6 @@
 #loading libraries:
 library(tidyverse)
+library(ggforce)
 library(GEOquery)
 library(gt)
 library(gtsummary)
@@ -37,7 +38,7 @@ colnames(pheno) <- rename_cols
 #pheno_rename <- pheno %>% gt()
 
 #selecting columns:
-pheno_bcr <- pheno %>% select(bcrRelapse, clinicalStage, tumorGleason)
+pheno_bcr <- pheno %>% dplyr::select(bcrRelapse, clinicalStage, tumorGleason)
 
 # summarize the data with our package
 pheno_summary <- tbl_summary(pheno_bcr)
@@ -128,7 +129,7 @@ pheno_table_gleason7 <- pheno %>% dplyr::select(bcrRelapse, tumorGleason) %>%
   group_by(bcrRelapse, tumorGleason) %>%
   summarize(n = n())
 
-pheno_pivot_table_gleason7 <- dcast(data = pheno_table
+pheno_pivot_table_gleason7 <- dcast(data = pheno_table_gleason7
                            ,formula = bcrRelapse ~ tumorGleason
                            ,fun.aggregate=sum
                            ,value.var="n")
@@ -169,3 +170,155 @@ topTable(fit2, coef=1, adjust="BH")
 #angle.
 
 #I need to do a boxplot of this gene in 3+4 vs 4+3 prostate cancer cases.
+#I need to transform data from wide to long
+twoProbes <- c("ILMN_2337740", "ILMN_1720965")
+twoProbes_idx <- which(rownames(exprsdat_gleason7) %in% twoProbes)
+exprsdat_gleason7_twoProbes <- exprsdat_gleason7[twoProbes_idx,]
+exprsdat_gleason7_twoProbes[,1:3]
+exprsdat_gleason7_twoProbes_long <- gather(data.frame(exprsdat_gleason7_twoProbes),
+                                           sampleID, logExprs)
+gleason7cat <- pheno_gleason7$tumorGleason
+gleason7cat <- gsub("7=3+4", "g3plus4", gleason7cat, fixed = TRUE)
+gleason7cat <- gsub("7=4+3", "g4plus3", gleason7cat, fixed = TRUE)
+
+probeID <- rep(rownames(exprsdat_gleason7_twoProbes), times = ncol(exprsdat_gleason7_twoProbes))
+condition <- rep(gleason7cat, times = ncol(exprsdat_gleason7_twoProbes))
+exprsdat_gleason7_twoProbes_long_probeID <- data.frame(probeID, 
+                                                       exprsdat_gleason7_twoProbes_long$sampleID,
+                                                       gleason7cat,
+                                                       exprsdat_gleason7_twoProbes_long$logExprs)
+colnames(exprsdat_gleason7_twoProbes_long_probeID) <- c("probeID", "sampleID", "gleason7cat", "logExprs")
+head(exprsdat_gleason7_twoProbes_long_probeID)
+pdf(file = "Graphs/ViolinPlotTwoTULP4probes.pdf"
+    , width = 11
+    , height = 8.5)
+ggplot(exprsdat_gleason7_twoProbes_long_probeID,
+       aes(probeID, logExprs, fill = gleason7cat, color = gleason7cat)) +
+  geom_violin() +
+  ggtitle("Log Expression of Two Probes Associated with TULP4 Gene")
+dev.off()
+#I don't think the probes are worth invesigating for survival analysis
+#now, I want to start looking at the genes from my dissertation. 
+#first, I have to link the probes to the gene symbols:
+probe_ids <- rownames(exprsdat_gleason7)
+# The below function call will return a datafram with probe_id, gene symbol
+# and ŕefgene_id for your data
+probeToSymbol_df <- select(illuminaHumanv4.db, 
+       keys = probe_ids, 
+       columns=c("SYMBOL"), 
+       keytype="PROBEID")
+genesOfInterest <- c("MYBPC1", "SLC7A4", "CDC42EP5", "BCAS1", "PAK1IP1", 
+                     "ANPEP", "SLC23A1", "HS.270778", "GLB1L2", "GCNT2",
+                     "TNFRSF19", "CUX2", "POTEG", "NCAPD3", "LOC728606", 
+                     "SLC22A3", "C9ORF61", "SRD5A2", "FLJ31568", "SERPINF2", 
+                     "PGM5", "LOC645993", "ZDHHC8P", "CNTNAP2", "F12", "CXCL14",
+                     "CTHRC1", "ZNF467", "CDC20")
+genes_idx <- which(probeToSymbol_df$SYMBOL %in% genesOfInterest)
+exprsdat_gleason7_idx <- which(rownames(exprsdat_gleason7) %in% probeToSymbol_df$PROBEID[genes_idx])
+exprsdat_gleason7_probesOfInterest <- exprsdat_gleason7[exprsdat_gleason7_idx,]
+rownames(exprsdat_gleason7_probesOfInterest) == probeToSymbol_df$PROBEID[genes_idx]
+#all are true! :)
+#I'm going to rename the rownames and merge probe ID to gene symbol together:
+probeID_geneSymbol <- paste0(rownames(exprsdat_gleason7_probesOfInterest), 
+                             ":", probeToSymbol_df$SYMBOL[genes_idx])
+exprsdat_gleason7_probesOfInterest_probeIDSymbol_merge <- exprsdat_gleason7_probesOfInterest
+rownames(exprsdat_gleason7_probesOfInterest_probeIDSymbol_merge) <- probeID_geneSymbol
+
+#going to perform differential gene analysis but focusing on the p-value, not adjusted p-value
+fit <- lmFit(exprsdat_gleason7_probesOfInterest_probeIDSymbol_merge, design)
+contrast.matrix <- makeContrasts("gleason3plus4 - gleason4plus3", levels=design)
+fit2 <- contrasts.fit(fit, contrast.matrix)
+fit2 <- eBayes(fit2)
+topTable(fit2, coef=1, adjust="BH")
+#okay so only SRD5A2 and PGM5 are differentially expressed between 4+3 vs 3+4
+#the unfortunate part is the log fold change is like 0.26 and 0.17 respectively
+#I'll take a look at these probes in the survival analysis.
+sample_idx <- which(rownames(pheno) %in% rownames(pheno_gleason7))
+pheno_survival <- pheno[sample_idx,]
+pheno_survival <- data.frame(sampleID, pheno_survival)
+#now, I need to add the probe expression to the pheno data as well:
+pheno_survival_withProbes <- data.frame(pheno_survival, t(exprsdat_gleason7_twoProbes), t(exprsdat_gleason7_probesOfInterest_probeIDSymbol_merge))
+colnames(pheno_survival_withProbes)[8:9] <- c("ILMN_1720965.TULP4", "ILMN_2337740.TULP4")
+pheno_survival_withProbes_tibble <- as_tibble(pheno_survival_withProbes)
+pheno_survival_withProbes_tibble$tumorGleason <-
+  case_match(
+  pheno_survival_withProbes_tibble$tumorGleason,
+  "7=3+4" ~ "g3+4",
+  "7=4+3" ~ "g4+3",
+)
+pheno_survival_withProbes_tibble$bcrRelapse <-
+  case_match(
+    pheno_survival_withProbes_tibble$bcrRelapse,
+    "Y" ~ TRUE,
+    "N" ~ FALSE,
+  )
+#before I do the survival analysis, let me first create violin plots:
+exprsdat_gleason7_allProbes_long <- gather(data.frame(t(pheno_survival_withProbes[ ,c(8:53)])),
+                                           sampleID, logExprs)
+gleason7cat <- pheno_survival_withProbes$tumorGleason
+gleason7cat <- gsub("7=3+4", "g3plus4", gleason7cat, fixed = TRUE)
+gleason7cat <- gsub("7=4+3", "g4plus3", gleason7cat, fixed = TRUE)
+gleason7cat <- rep(gleason7cat, times = ncol(pheno_survival_withProbes[ ,c(8:53)]))
+probeID <- rep(colnames(pheno_survival_withProbes)[8:53], times = nrow(pheno_survival_withProbes))
+exprsdat_gleason7_allProbes_long_probeID <- data.frame(probeID, 
+                                                       exprsdat_gleason7_allProbes_long$sampleID,
+                                                       gleason7cat,
+                                                       exprsdat_gleason7_allProbes_long$logExprs)
+colnames(exprsdat_gleason7_allProbes_long_probeID) <- c("probeID", "sampleID", "gleason7cat", "logExprs")
+head(exprsdat_gleason7_allProbes_long_probeID)
+pdf(file = "Graphs/ViolinPlotsAllprobes.pdf"
+    ,onefile = TRUE
+    , width = 11
+    , height = 8.5)
+numOfPages <- round( (ncol(pheno_survival_withProbes[ ,c(8:53)]) / 3) + 0.499999)
+for (currentPage in 1:numOfPages){
+  print(ggplot(exprsdat_gleason7_allProbes_long_probeID,
+         aes(x = probeID, y = logExprs, fill = gleason7cat, color = gleason7cat)) +
+    geom_violin() +
+    facet_wrap_paginate( ~ probeID, scales = "free_x", ncol = 3, nrow = 1, page = currentPage) +
+    ggtitle("Log Expression of All Probes Associated with Key Genes"))
+}
+dev.off()
+#I cannot perform a survival analysis based on the expression of the probes.
+#I would need to establish a cut off for expression high and expression low for 
+#each of the probes and I cannto do this since the expression profiles are
+#so close. Instead, I will perform survival analysis based on biochemical
+#recurrence and Gleason score.
+pheno_survival_withProbes_tibble$timeToRecurrence <- as.numeric(pheno_survival_withProbes_tibble$timeToRecurrence)
+s <- survfit(Surv(timeToRecurrence, bcrRelapse) ~ tumorGleason, data = pheno_survival_withProbes_tibble)
+plot(s)
+pdf(file = "Graphs/KaplanMeierPlots.pdf"
+    ,onefile = TRUE
+    , width = 11
+    , height = 8.5)
+ggsurvplot(s, data = pheno_survival_withProbes_tibble,
+           pval = TRUE, risk.table=TRUE,
+           legend.labs=c("Gleason3+4","Gleason4+3"),
+           title="Kaplan-Meier Curve for Prostate Cancer Cases with Biochemical Recurrence",
+           risk.table.height=0.15)
+dev.off()
+#There is no difference between the two groups. It doesn't matter if the 
+#patient has Gleason 3+4 or Gleason 4+3. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
